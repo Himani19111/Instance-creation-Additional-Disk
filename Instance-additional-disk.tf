9cfa51681c2029d03594e15af222e31abfd3313d
Himani19111 <skdigolia18@gmail.com>
2025-04-13 20:00:00 +0530

    changes code

--- Instance-additional-disk.tf
+++ Instance-additional-disk.tf
@@ -6,9 +6,9 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-0e35ddab05955cf57"  # Example AMI (use a valid one for your region)
   instance_type = "t2.micro"
 
-  root_block_device {
-    volume_size = 8  # Root disk size (optional, default is usually 8 GB)
-  }
+#  root_block_device {
+#    volume_size = 8  # Root disk size (optional, default is usually 8 GB)
+#  }
 
   tags = {
     Name = "Instance-With-Extra-Disk"
@@ -16,19 +16,19 @@ resource "aws_instance" "my_instance" {
 }
 
 # ➕ Additional EBS Volume (20GB)
-resource "aws_ebs_volume" "extra_disk" {
-  availability_zone = aws_instance.my_instance.availability_zone
-  size              = 20  # Size in GB
-  type              = "gp2"  # General Purpose SSD
-
-  tags = {
-    Name = "Extra-20GB-Disk"
-  }
-}
+#resource "aws_ebs_volume" "extra_disk" {
+#  availability_zone = aws_instance.my_instance.availability_zone
+#  size              = 20  # Size in GB
+#  type              = "gp2"  # General Purpose SSD
+#
+#  tags = {
+#    Name = "Extra-20GB-Disk"
+#  }
+#}
 
 # 🔗 Attach the EBS Volume to the instance
-resource "aws_volume_attachment" "ebs_attach" {
-  device_name = "/dev/sdf"  # This is how Linux will see it
-  volume_id   = aws_ebs_volume.extra_disk.id
-  instance_id = aws_instance.my_instance.id
-}
+#resource "aws_volume_attachment" "ebs_attach" {
+#  device_name = "/dev/sdf"  # This is how Linux will see it
+#  volume_id   = aws_ebs_volume.extra_disk.id
+#  instance_id = aws_instance.my_instance.id
+#}
